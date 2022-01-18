4f523991bfe9455f9ee9833cc5658cbe8ad55809
yann <yann.coornart@ext.leroymerlin.fr>
2022-01-18 10:53:39 +0100

    Feat(): Added vertica batch

---
+++ plugin-jdbc-vertica/src/test/java/io/kestra/plugin/jdbc/vertica/BatchTest.java
@@ -0,0 +1,179 @@
+package io.kestra.plugin.jdbc.vertica;
+
+import com.google.common.collect.ImmutableMap;
+import io.kestra.core.runners.RunContext;
+import io.kestra.core.serializers.FileSerde;
+import io.kestra.core.utils.IdUtils;
+import io.kestra.plugin.jdbc.AbstractJdbcBatch;
+import io.kestra.plugin.jdbc.AbstractRdbmsTest;
+import io.micronaut.context.annotation.Value;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
+import java.math.BigDecimal;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.SQLException;
+import java.sql.Time;
+import java.time.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@MicronautTest
+public class BatchTest extends AbstractRdbmsTest {
+
+    @Test
+    void insert() throws Exception {
+        RunContext runContext = runContextFactory.of(ImmutableMap.of());
+
+        File tempFile = File.createTempFile(this.getClass().getSimpleName().toLowerCase() + "_", ".trs");
+        OutputStream output = new FileOutputStream(tempFile);
+
+        for (int i = 0; i < 5; i++) {
+            FileSerde.write(output, List.of(
+                new byte[]{0b000101},
+                new byte[]{0b000101},
+                new byte[]{0b000101},
+                new byte[]{0b000101},
+                new byte[]{0b000101},
+                true,
+                "four",
+                "four",
+                "four",
+                LocalDate.parse("2030-12-25"),
+                Time.valueOf(LocalTime.parse("04:05:30")),
+                LocalDateTime.parse("2004-10-19T10:23:54.999999"),
+                LocalDateTime.parse("2004-10-19T10:23:54.999999"),
+                OffsetTime.parse("13:05:06+01:00"),
+                LocalDateTime.parse("2004-10-19T10:23:54.999999"),
+                ZonedDateTime.parse("1999-01-08T13:05:06+01:00[Europe/Paris]"),
+//                Duration.parse("PT719H59M59S"),
+//                Duration.parse("PT4H54.000775807S"),
+//                Period.parse("P1Y6M"),
+                1.2F,
+                2147483645.1234,
+                2147483645.1234,
+                2147483645.1234,
+                2147483645.1234,
+                123456789L,
+                123456789L,
+                123456789L,
+                123456789L,
+                123L,
+                1L,
+                new BigDecimal("2147483645.1234"),
+                new BigDecimal("2147483645.1234"),
+                new BigDecimal("2147483645.1234"),
+                new BigDecimal("2147483645.1234")
+            ));
+        }
+
+        URI uri = storageInterface.put(URI.create("/" + IdUtils.create() + ".ion"), new FileInputStream(tempFile));
+
+        Batch task = Batch.builder()
+            .url(getUrl())
+            .username(getUsername())
+            .password(getPassword())
+            .from(uri.toString())
+            .sql("insert into vertica_types values( ? , ? , ? , ? , ? , ? , ? , ? , ?, ? , ? , ? , ? , ? , ? , ?, ? , ? , ? , ? , ?  , ? , ? , ? , ? , ?  , ? ,  ?  , ? , ? , ? )")
+            .build();
+
+        AbstractJdbcBatch.Output runOutput = task.run(runContext);
+    }
+
+    @Test
+    public void namedInsert() throws Exception {
+        RunContext runContext = runContextFactory.of(ImmutableMap.of());
+
+
+        File tempFile = File.createTempFile(this.getClass().getSimpleName().toLowerCase() + "_", ".trs");
+        OutputStream output = new FileOutputStream(tempFile);
+
+        for (int i = 0; i < 5; i++) {
+            FileSerde.write(output, ImmutableMap.builder()
+                .put("id", 125)
+                .put("name", "kestra")
+                .put("address", "here")
+                .build()
+            );
+
+        }
+
+
+        URI uri = storageInterface.put(URI.create("/" + IdUtils.create() + ".ion"), new FileInputStream(tempFile));
+
+        ArrayList<String> columns = new ArrayList<>();
+        columns.add("id");
+        columns.add("name");
+        columns.add("address");
+
+        Batch task = Batch.builder()
+            .url(getUrl())
+            .username(getUsername())
+            .password(getPassword())
+            .from(uri.toString())
+            .sql("insert into namedInsert values( ? , ? , ? )")
+            .build();
+
+        AbstractJdbcBatch.Output runOutput = task.run(runContext);
+    }
+
+    @Test
+    public void namedColumnsInsert() throws Exception {
+        RunContext runContext = runContextFactory.of(ImmutableMap.of());
+
+
+        File tempFile = File.createTempFile(this.getClass().getSimpleName().toLowerCase() + "_", ".trs");
+        OutputStream output = new FileOutputStream(tempFile);
+
+        for (int i = 0; i < 5; i++) {
+            FileSerde.write(output, ImmutableMap.builder()
+                .put("id", 125)
+                .put("name", "kestra")
+                .put("address", "here")
+                .build()
+            );
+
+        }
+
+
+        URI uri = storageInterface.put(URI.create("/" + IdUtils.create() + ".ion"), new FileInputStream(tempFile));
+
+        ArrayList<String> columns = new ArrayList<>();
+        columns.add("id");
+        columns.add("name");
+
+        Batch task = Batch.builder()
+            .url(getUrl())
+            .username(getUsername())
+            .password(getPassword())
+            .from(uri.toString())
+            .sql("insert into namedInsert(id,name) values( ? , ? )")
+            .columns(columns)
+            .build();
+
+        AbstractJdbcBatch.Output runOutput = task.run(runContext);
+    }
+
+
+    @Override
+    protected String getUrl() {
+        return "jdbc:vertica://127.0.0.1:25433/docker";
+    }
+
+    @Override
+    protected String getUsername() {
+        return "dbadmin";
+    }
+
+    @Override
+    protected String getPassword() {
+        return "vertica_passwd";
+    }
+
+    @Override
+    protected void initDatabase() throws SQLException, FileNotFoundException, URISyntaxException {
+        executeSqlScript("scripts/vertica_insert.sql");
+    }
+}
---
+++ plugin-jdbc-vertica/src/test/resources/scripts/vertica_insert.sql
@@ -0,0 +1,48 @@
+
+DROP TABLE IF EXISTS vertica_types;
+DROP TABLE IF EXISTS namedInsert;
+CREATE TABLE vertica_types (
+   "binary" BINARY,
+   varbinary VARBINARY,
+   long_varbinary LONG VARBINARY,
+   bytea BYTEA,
+   raw RAW,
+   "boolean" BOOLEAN,
+   char CHAR(4),
+   varchar VARCHAR,
+   long_varchar LONG VARCHAR,
+   date DATE,
+   time TIME,
+   datetime DATETIME,
+   smalldatetime SMALLDATETIME,
+   time_with_timezone TIME WITH TIMEZONE,
+   timestamp TIMESTAMP,
+   timestamp_with_timezone TIMESTAMP WITH TIMEZONE,
+--   "interval" INTERVAL,
+--   interval_day_to_second INTERVAL DAY TO SECOND,
+--   interval_year_to_month INTERVAL YEAR TO MONTH,
+   double_precision DOUBLE PRECISION,
+   float FLOAT,
+   float_n FLOAT(8),
+   float8 FLOAT8,
+   real REAL,
+   integer INTEGER,
+   int INT,
+   bigint BIGINT,
+   int8 INT8,
+   smallint SMALLINT,
+   tinyint TINYINT,
+   decimal DECIMAL(14, 4),
+   numeric NUMERIC(14, 4),
+   number NUMBER(14, 4),
+   money MONEY(14, 4)
+--   geometry GEOMETRY,
+--   geography GEOGRAPHY,
+--   uuid UUID
+);
+
+CREATE TABLE namedInsert (
+ id integer,
+ name VARCHAR,
+ address VARCHAR
+ );
