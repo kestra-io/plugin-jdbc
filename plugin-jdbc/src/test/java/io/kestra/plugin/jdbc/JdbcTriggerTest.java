361e3b4e3ad03ec296335beceb0624cb646b877d
brian-mulier-p <bmmulier@hotmail.fr>
2023-06-23 16:15:12 +0200

    fix(jdbc): error on sql trigger without result (#133)
    closes #129

---
+++ plugin-jdbc/src/test/java/io/kestra/plugin/jdbc/JdbcTriggerTest.java
@@ -0,0 +1,44 @@
+package io.kestra.plugin.jdbc;
+
+import io.kestra.core.models.conditions.ConditionContext;
+import io.kestra.core.models.executions.Execution;
+import io.kestra.core.models.triggers.TriggerContext;
+import io.kestra.core.runners.RunContext;
+import io.micronaut.context.ApplicationContext;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+import java.util.Optional;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+@MicronautTest
+public class JdbcTriggerTest {
+    @Inject
+    protected ApplicationContext applicationContext;
+
+    @Test
+    void noNpeOnNullSizeQueryOutput() throws Exception {
+        Optional<Execution> evaluate = new DoNothingTrigger().evaluate(
+            ConditionContext.builder().runContext(
+                new RunContext(applicationContext, Collections.emptyMap())
+            ).build(),
+            new TriggerContext()
+        );
+
+        assertThat(evaluate.isEmpty(), is(true));
+    }
+
+    private static class DoNothingTrigger extends AbstractJdbcTrigger {
+        @Override
+        public void registerDriver() {}
+
+        @Override
+        protected AbstractJdbcQuery.Output runQuery(RunContext runContext) {
+            return AbstractJdbcQuery.Output.builder().build();
+        }
+    }
+}
