971a259515297bb4de6f3148579211c6f21ec5f9
Loïc Mathieu <loikeseke@gmail.com>
2024-04-04 17:39:00 +0200

    feat: AS400 plugin
    Fixes #248

---
+++ plugin-jdbc-as400/src/main/java/io/kestra/plugin/jdbc/db2/As400CellConverter.java
@@ -0,0 +1,41 @@
+package io.kestra.plugin.jdbc.db2;
+
+import io.kestra.plugin.jdbc.AbstractCellConverter;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.ZoneId;
+
+/**
+ * Copied from the DB2 code as we cannot test AS400 we assume it works like DB2
+ */
+public class As400CellConverter extends AbstractCellConverter {
+    public As400CellConverter(ZoneId zoneId) {
+        super(zoneId);
+    }
+
+    @Override
+    public Object convertCell(int columnIndex, ResultSet rs, Connection connection) throws SQLException {
+        Object data = rs.getObject(columnIndex);
+
+        if (data == null) {
+            return null;
+        }
+
+        String columnTypeName = rs.getMetaData().getColumnTypeName(columnIndex);
+
+	    return switch (columnTypeName.toLowerCase()) {
+		    case "char", "varchar" -> ((com.ibm.as400.access.AS400JDBCResultSet) rs).getString(columnIndex);
+		    case "date" -> ((com.ibm.as400.access.AS400JDBCResultSet)  rs).getDate(columnIndex).toLocalDate();
+		    case "time" -> ((com.ibm.as400.access.AS400JDBCResultSet)  rs).getTime(columnIndex).toLocalTime();
+		    case "timestamp" -> ((com.ibm.as400.access.AS400JDBCResultSet) rs).getTimestamp(columnIndex).toInstant();
+		    case "blob" -> ((com.ibm.as400.access.AS400JDBCResultSet)  rs).getBlob(columnIndex);
+		    case "clob" -> ((com.ibm.as400.access.AS400JDBCResultSet) rs).getClob(columnIndex);
+		    case "nclob" -> ((com.ibm.as400.access.AS400JDBCResultSet)  rs).getNClob(columnIndex);
+		    case "xml" -> ((com.ibm.as400.access.AS400JDBCResultSet)  rs).getSQLXML(columnIndex);
+		    default -> super.convert(columnIndex, rs);
+	    };
+
+    }
+}
---
+++ plugin-jdbc-as400/src/main/java/io/kestra/plugin/jdbc/db2/Trigger.java
@@ -0,0 +1,83 @@
+package io.kestra.plugin.jdbc.db2;
+
+import io.kestra.core.models.annotations.Example;
+import io.kestra.core.models.annotations.Plugin;
+import io.kestra.core.runners.RunContext;
+import io.kestra.plugin.jdbc.AbstractJdbcQuery;
+import io.kestra.plugin.jdbc.AbstractJdbcTrigger;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * Copied from the DB2 code as we cannot test AS400 we assume it works like DB2
+ */
+@SuperBuilder
+@ToString
+@EqualsAndHashCode
+@Getter
+@NoArgsConstructor
+@Schema(
+    title = "Wait for query on a AS400 database."
+)
+@Plugin(
+    examples = {
+        @Example(
+            title = "Wait for a SQL query to return results, and then iterate through rows.",
+            full = true,
+            code = {
+                "id: jdbc-trigger",
+                "namespace: io.kestra.tests",
+                "",
+                "tasks:",
+                "  - id: each",
+                "    type: io.kestra.core.tasks.flows.EachSequential",
+                "    tasks:",
+                "      - id: return",
+                "        type: io.kestra.core.tasks.debugs.Return",
+                "        format: \"{{ json(taskrun.value) }}\"",
+                "    value: \"{{ trigger.rows }}\"",
+                "",
+                "triggers:",
+                "  - id: watch",
+                "    type: io.kestra.plugin.jdbc.as400.Trigger",
+                "    interval: \"PT5M\"",
+                "    url: jdbc:as400://127.0.0.1:50000/",
+                "    username: as400_user",
+                "    password: as400_passwd",
+                "    sql: \"SELECT * FROM my_table\"",
+                "    fetch: true",
+            }
+        )
+    }
+)
+public class Trigger extends AbstractJdbcTrigger {
+
+    @Override
+    protected AbstractJdbcQuery.Output runQuery(RunContext runContext) throws Exception {
+
+        var query = Query.builder()
+            .id(this.id)
+            .type(Query.class.getName())
+            .url(this.getUrl())
+            .username(this.getUsername())
+            .password(this.getPassword())
+            .timeZoneId(this.getTimeZoneId())
+            .sql(this.getSql())
+            .fetch(this.isFetch())
+            .store(this.isStore())
+            .fetchOne(this.isFetchOne())
+            .additionalVars(this.additionalVars)
+            .build();
+
+        return query.run(runContext);
+    }
+
+    @Override
+    public void registerDriver() throws SQLException {
+        DriverManager.registerDriver(new com.ibm.as400.access.AS400JDBCDriver());
+    }
+}
