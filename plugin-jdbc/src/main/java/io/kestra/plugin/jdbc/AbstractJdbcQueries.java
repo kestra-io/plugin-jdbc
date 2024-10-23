8da6ac39a76d57166d6cb45721afc867c67b175a
Mathieu Gabelle <mgabelle@kestra.io>
2024-10-23 08:58:27 +0200

    fix: javadoc issue with lombok builder as parameter

--- plugin-jdbc/src/main/java/io/kestra/plugin/jdbc/AbstractJdbcQueries.java
+++ plugin-jdbc/src/main/java/io/kestra/plugin/jdbc/AbstractJdbcQueries.java
@@ -4,15 +4,16 @@ import io.kestra.core.exceptions.IllegalVariableEvaluationException;
 import io.kestra.core.models.executions.metrics.Counter;
 import io.kestra.core.models.property.Property;
 import io.kestra.core.runners.RunContext;
+import io.kestra.core.serializers.FileSerde;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 import org.slf4j.Logger;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.sql.*;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.function.Consumer;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -62,7 +63,33 @@ public abstract class AbstractJdbcQueries extends AbstractJdbcBaseQuery implemen
                         //When sql is not a select statement skip output creation
                         if(rs != null) {
                             AbstractJdbcQuery.Output.OutputBuilder<?, ?> output = AbstractJdbcQuery.Output.builder();
-                            totalSize += populateOutputFromResultSet(runContext, stmt, rs, output, cellConverter, conn);
+                            //Populate result fro result set
+                            long size = 0L;
+                            switch (this.getFetchType()) {
+                                case FETCH_ONE -> {
+                                    size = 1L;
+                                    output
+                                        .row(fetchResult(rs, cellConverter, conn))
+                                        .size(size);
+                                }
+                                case STORE -> {
+                                    File tempFile = runContext.workingDir().createTempFile(".ion").toFile();
+                                    try (BufferedWriter fileWriter = new BufferedWriter(new FileWriter(tempFile), FileSerde.BUFFER_SIZE)) {
+                                        size = fetchToFile(stmt, rs, fileWriter, cellConverter, conn);
+                                    }
+                                    output
+                                        .uri(runContext.storage().putFile(tempFile))
+                                        .size(size);
+                                }
+                                case FETCH -> {
+                                    List<Map<String, Object>> maps = new ArrayList<>();
+                                    size = fetchResults(stmt, rs, maps, cellConverter, conn);
+                                    output
+                                        .rows(maps)
+                                        .size(size);
+                                }
+                            }
+                            totalSize += size;
                             outputList.add(output.build());
                         }
                     }
