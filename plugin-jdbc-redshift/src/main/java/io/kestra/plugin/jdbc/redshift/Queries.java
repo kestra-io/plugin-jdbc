04d94ff2da29590ea79249f9f35d511a8488dd50
Mathieu Gabelle <54168385+mgabelle@users.noreply.github.com>
2024-10-31 11:35:42 +0100

    feat: add queries : redshift, snowflake, sybase, vectorwise, vertica (#424)

---
+++ plugin-jdbc-redshift/src/main/java/io/kestra/plugin/jdbc/redshift/Queries.java
@@ -0,0 +1,59 @@
+package io.kestra.plugin.jdbc.redshift;
+
+import io.kestra.core.models.annotations.Example;
+import io.kestra.core.models.annotations.Plugin;
+import io.kestra.core.models.tasks.RunnableTask;
+import io.kestra.plugin.jdbc.AbstractCellConverter;
+import io.kestra.plugin.jdbc.AbstractJdbcQueries;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.time.ZoneId;
+
+@SuperBuilder
+@ToString
+@EqualsAndHashCode
+@Getter
+@NoArgsConstructor
+@Schema(
+    title = "Perform multiple queries on a Redshift server."
+)
+@Plugin(
+    examples = {
+        @Example(
+            title = "Send SQL queries to a Redshift database and fetch the outputs.",
+            full = true,
+            code = """
+                   id: redshift_queries
+                   namespace: company.team
+
+                   tasks:
+                     - id: select
+                       type: io.kestra.plugin.jdbc.redshift.Queries
+                       url: jdbc:redshift://123456789.eu-central-1.redshift-serverless.amazonaws.com:5439/dev
+                       username: admin
+                       password: admin_password
+                       sql: select count(*) from employee; select count(*) from laptop;
+                       fetchType: FETCH_ONE
+                   """
+        )
+    }
+)
+public class Queries extends AbstractJdbcQueries implements RunnableTask<AbstractJdbcQueries.MultiQueryOutput> {
+    @Override
+    protected AbstractCellConverter getCellConverter(ZoneId zoneId) {
+        return new RedshiftCellConverter(zoneId);
+    }
+
+    @Override
+    public void registerDriver() throws SQLException {
+        DriverManager.registerDriver(new com.amazon.redshift.jdbc.Driver());
+    }
+
+}
---
+++ plugin-jdbc-vertica/src/main/java/io/kestra/plugin/jdbc/vertica/Queries.java
@@ -0,0 +1,58 @@
+package io.kestra.plugin.jdbc.vertica;
+
+import io.kestra.core.models.annotations.Example;
+import io.kestra.core.models.annotations.Plugin;
+import io.kestra.core.models.tasks.RunnableTask;
+import io.kestra.plugin.jdbc.AbstractCellConverter;
+import io.kestra.plugin.jdbc.AbstractJdbcQueries;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.time.ZoneId;
+
+@SuperBuilder
+@ToString
+@EqualsAndHashCode
+@Getter
+@NoArgsConstructor
+@Schema(
+    title = "Perform multiple queries on a Vertica server."
+)
+@Plugin(
+    examples = {
+        @Example(
+            title = "Send SQL queries to a Vertica database, and fetch a row as output.",
+            full = true,
+            code = """
+                   id: vertica_queries
+                   namespace: company.team
+
+                   tasks:
+                     - id: queries
+                       type: io.kestra.plugin.jdbc.vertica.Queries
+                       url: jdbc:vertica://127.0.0.1:56982/db
+                       username: vertica_user
+                       password: vertica_password
+                       sql: select * from customer
+                       fetchType: FETCH_ONE
+                   """
+        )
+    }
+)
+public class Queries extends AbstractJdbcQueries implements RunnableTask<AbstractJdbcQueries.MultiQueryOutput> {
+    @Override
+    protected AbstractCellConverter getCellConverter(ZoneId zoneId) {
+        return new VerticaCellConverter(zoneId);
+    }
+
+    @Override
+    public void registerDriver() throws SQLException {
+        DriverManager.registerDriver(new com.vertica.jdbc.Driver());
+    }
+}
