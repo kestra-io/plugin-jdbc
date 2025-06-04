d06610a1e70914a33dffee9d72e55b724dffcc6c
François Delbrayelle <fdelbrayelle@gmail.com>
2025-06-04 22:02:01 +0200

    chore(deps): bump clickhouse-jdbc to 0.8.6 (#614)

--- plugin-jdbc-clickhouse/src/main/java/io/kestra/plugin/jdbc/clickhouse/ClickHouseCellConverter.java
+++ plugin-jdbc-clickhouse/src/main/java/io/kestra/plugin/jdbc/clickhouse/ClickHouseCellConverter.java
@@ -1,18 +1,19 @@
 package io.kestra.plugin.jdbc.clickhouse;
 
 import com.clickhouse.data.value.UnsignedLong;
-import com.clickhouse.data.value.UnsignedShort;
 import io.kestra.plugin.jdbc.AbstractCellConverter;
 
+import java.math.BigInteger;
 import java.net.Inet4Address;
 import java.net.Inet6Address;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.regex.Matcher;
+import java.util.Arrays;
 import java.util.regex.Pattern;
 
 public class ClickHouseCellConverter extends AbstractCellConverter {
@@ -34,42 +35,53 @@ public class ClickHouseCellConverter extends AbstractCellConverter {
         Object columnVal = rs.getObject(columnIndex);
         String columnTypeName = rs.getMetaData().getColumnTypeName(columnIndex);
 
-        if (columnTypeName.equals("DateTime")) {
-            // date with no TZ, we use the server default one
-            return LocalDateTime
-                .parse(
-                    rs.getString(columnIndex),
-                    DATE_TIME_FORMAT
-                );
-        } else if (columnTypeName.startsWith("DateTime")) {
-            Matcher matcher = PATTERN.matcher(columnTypeName);
-            if (!matcher.find() || matcher.groupCount() < 3) {
-                throw new IllegalArgumentException("Invalid column type '" + columnTypeName + "'");
+        if (columnTypeName.equals("Array(Int8)")) {
+            Object raw = rs.getObject(columnIndex);
+            if (raw instanceof java.sql.Array sqlArray) {
+                Object arrayObj = sqlArray.getArray();
+                if (arrayObj instanceof Object[] objArray) {
+                    Byte[] byteArray = new Byte[objArray.length];
+                    for (int i = 0; i < objArray.length; i++) {
+                        byteArray[i] = ((Number) objArray[i]).byteValue();
+                    }
+                    return byteArray;
+                }
             }
-
-            return LocalDateTime
-                .parse(
-                    rs.getString(columnIndex),
-                    DATE_TIME_FORMAT
-                )
-                .atZone(ZoneId.of(matcher.group(3)))
-                .withZoneSameInstant(zoneId);
-        }
-
-        if (columnTypeName.equals("Int8")) {
-            Byte col = (Byte) columnVal;
-            return col.intValue();
-        }
-
-        if (columnTypeName.equals("Date")) {
-            return columnVal;
         }
 
         if (columnTypeName.startsWith("Array(")) {
+            Object value = rs.getObject(columnIndex);
+
+            if (value instanceof java.sql.Array) {
+                return ((java.sql.Array) value).getArray();
+            }
+            return value;
+        }
+
+        if (columnTypeName.equals("DateTime")) {
+            return rs.getTimestamp(columnIndex).toLocalDateTime();
+        } else if (columnTypeName.startsWith("DateTime(") || columnTypeName.startsWith("DateTime64(")) {
+            return rs.getObject(columnIndex, ZonedDateTime.class);
+        }
+
+        if (columnTypeName.equals("Int8")) {
             return columnVal;
         }
 
+        if (columnTypeName.equals("Date")) {
+            if (columnVal instanceof java.sql.Date) {
+                return ((java.sql.Date) columnVal).toLocalDate();
+            } else if (columnVal instanceof LocalDate) {
+                return columnVal;
+            } else {
+                return LocalDate.parse(columnVal.toString());
+            }
+        }
+
         if (columnTypeName.startsWith("Tuple(")) {
+            if (columnVal instanceof Object[]) {
+                return Arrays.asList((Object[]) columnVal);
+            }
             return columnVal;
         }
 
@@ -84,13 +96,28 @@ public class ClickHouseCellConverter extends AbstractCellConverter {
         }
 
         if (columnTypeName.equals("UInt64")) {
-            UnsignedLong col = (UnsignedLong) columnVal;
-            return col.longValue();
+            return switch (columnVal) {
+                case UnsignedLong unsigned -> unsigned.longValue();
+                case BigInteger bigInt -> bigInt.longValue();
+                case Number number -> number.longValue();
+                default -> throw new IllegalArgumentException("Unexpected type for UInt64: " + columnVal.getClass());
+            };
         }
 
         if (columnTypeName.equals("UInt16")) {
-            UnsignedShort col = (UnsignedShort) columnVal;
-            return col.intValue();
+            if (columnVal instanceof Number) {
+                return ((Number) columnVal).intValue();
+            } else {
+                throw new IllegalArgumentException("Unexpected type for UInt16: " + columnVal.getClass());
+            }
+        }
+
+        if ("Float32".equals(columnTypeName)) {
+            return rs.getFloat(columnIndex);
+        }
+
+        if ("Float64".equals(columnTypeName)) {
+            return rs.getDouble(columnIndex);
         }
 
         return super.convert(columnIndex, rs);
