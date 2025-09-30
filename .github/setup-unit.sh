456c56ac81e35886785eb3710dbd0e787216f0a2
Malaydewangan09 <malaydewangan310@gmail.com>
2025-09-30 11:40:17 +0530

    ci(): remove bitnami postgres image

--- .github/setup-unit.sh
+++ .github/setup-unit.sh
@@ -6,8 +6,8 @@ openssl genrsa -des3 -out certs/server/server.key -passout pass:p4ssphrase 2048
 openssl rsa -in certs/server/server.key -passin pass:p4ssphrase -out certs/server/server.key
 openssl req -new -nodes -key certs/server/server.key -out certs/server/server.csr -subj "/CN=postgresql"
 openssl x509 -req -in certs/server/server.csr -days 365 -CA certs/ca.crt -CAkey certs/ca.key -CAcreateserial -out certs/server/server.crt
-sudo chmod -R 775 certs/server/
-sudo chown -R 1001 certs/server/
+sudo chmod 600 certs/server/server.key
+sudo chown 999:999 certs/server/server.key
 
 mkdir certs/client
 openssl genrsa -des3 -out certs/client/client.key -passout pass:p4ssphrase 2048
@@ -15,11 +15,29 @@ openssl rsa -in certs/client/client.key -passin pass:p4ssphrase -out certs/clien
 openssl req -new -nodes -key certs/client/client.key -passin pass:p4ssphrase -out certs/client/client.csr -subj "/CN=postgres"
 openssl x509 -req -in certs/client/client.csr -days 365 -CA certs/ca.crt -CAkey certs/ca.key -CAcreateserial -out certs/client/client.crt
 
-mkdir plugin-jdbc-postgres/src/test/resources/ssl/
-cp certs/client/* plugin-jdbc-postgres/src/test/resources/ssl/
-cp certs/ca.crt plugin-jdbc-postgres/src/test/resources/ssl/
+SSL_RESOURCES="plugin-jdbc-postgres/src/test/resources/ssl"
+mkdir -p "$SSL_RESOURCES"
 
-docker compose -f docker-compose-ci.yml up --quiet-pull -d mariadb sqlserver
+cp certs/client/* "$SSL_RESOURCES/"
+cp certs/ca.crt "$SSL_RESOURCES/"
+
+cat > "$SSL_RESOURCES/postgresql.conf" <<'EOF'
+listen_addresses = '*'
+port = 5432
+
+ssl = on
+ssl_cert_file = '/var/lib/postgresql/server.crt'
+ssl_key_file  = '/var/lib/postgresql/server.key'
+ssl_ca_file   = '/var/lib/postgresql/ca.crt'
+EOF
+
+cat > "$SSL_RESOURCES/pg_hba.conf" <<'EOF'
+hostssl all all 0.0.0.0/0 md5
+hostssl all all ::/0      md5
+local   all all trust
+EOF
+
+docker compose -f docker-compose-ci.yml up --quiet-pull -d mariadb sqlserver postgres
 docker compose -f docker-compose-ci.yml up --quiet-pull -d --wait
 sleep 3
 
