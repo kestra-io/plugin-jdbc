ec665a9b2135fe37d3a94a891dd867e618b3493b
suraj a <96696731+suraj-0628@users.noreply.github.com>
2025-12-05 03:38:58 +0000

    test(duckdb): add DuckDbTestUtils helper for uploading CSV and DB file

---
+++ plugin-jdbc-hana/src/main/java/io/kestra/plugin/jdbc/hana/package-info.java
@@ -0,0 +1,7 @@
+@PluginSubGroup(
+    description = "This sub-group contains tasks for accessing SAP HANA databases.",
+    categories = PluginSubGroup.PluginCategory.DATABASE
+)
+package io.kestra.plugin.jdbc.hana;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
