58fa065f0ecad86efa41715548993bc53183976d
François Delbrayelle <fdelbrayelle@gmail.com>
2025-12-11 14:26:29 +0100

    Revert "try the SQLDialect.DEFAULT"
    This reverts commit ec380fbb1aaf8d163f6647e47509ed4c45d30ceb.

--- plugin-jdbc/src/main/java/io/kestra/plugin/jdbc/AbstractJdbcBaseQuery.java
+++ plugin-jdbc/src/main/java/io/kestra/plugin/jdbc/AbstractJdbcBaseQuery.java
@@ -230,7 +230,7 @@ public abstract class AbstractJdbcBaseQuery extends Task implements JdbcQueryInt
     }
 
     protected static String[] getQueries(String sql, SQLDialect dialect) {
-        Queries queries = DSL.using(SQLDialect.DEFAULT)
+        Queries queries = DSL.using(dialect)
             .parser()
             .parse(sql);
 
