71986b40cb918911c784a5ad4edd1ef2a3915287
Pradumna Saraf <pradumnasaraf@gmail.com>
2025-12-22 14:50:39 +0530

    test: add ClickHouse Runner tests

--- plugin-jdbc-clickhouse/src/test/java/io/kestra/plugin/jdbc/clickhouse/RunnerTest.java
+++ plugin-jdbc-clickhouse/src/test/java/io/kestra/plugin/jdbc/clickhouse/RunnerTest.java
@@ -1,10 +1,9 @@
-package io.kestra.plugin.jdbc.oracle;
+package io.kestra.plugin.jdbc.clickhouse;
 
 import io.kestra.core.junit.annotations.ExecuteFlow;
 import io.kestra.core.junit.annotations.KestraTest;
 import io.kestra.core.models.executions.Execution;
 import io.kestra.core.models.flows.State;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -15,12 +14,8 @@ import static org.hamcrest.Matchers.is;
 class RunnerTest {
 
     @Test
-    @ExecuteFlow("sanity-checks/all_oracle.yaml")
-    @Disabled("""
-            Because the runner is full:
-            Could not pull image: write /var/lib/docker/tmp/GetImageBlob1934987450: no space left on device
-        """)
-    void all_oracle(Execution execution) {
+    @ExecuteFlow("sanity-checks/all_clickhouse.yaml")
+    void all_clickhouse(Execution execution) {
         assertThat(execution.getTaskRunList(), hasSize(12));
         assertThat(execution.getState().getCurrent(), is(State.Type.SUCCESS));
     }
