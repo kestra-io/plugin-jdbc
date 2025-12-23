5b0b32d12b43ca78e8962aa46fd46a226e1a7c77
Pradumna Saraf <pradumnasaraf@gmail.com>
2025-12-23 17:39:56 +0530

    fix: update trino test to 10 tasks

--- plugin-jdbc-trino/src/test/java/io/kestra/plugin/jdbc/trino/RunnerTest.java
+++ plugin-jdbc-trino/src/test/java/io/kestra/plugin/jdbc/trino/RunnerTest.java
@@ -16,7 +16,7 @@ class RunnerTest {
     @Test
     @ExecuteFlow("sanity-checks/all_trino.yaml")
     void all_trino(Execution execution) {
-        assertThat(execution.getTaskRunList(), hasSize(11));
+        assertThat(execution.getTaskRunList(), hasSize(10));
         assertThat(execution.getState().getCurrent(), is(State.Type.SUCCESS));
     }
 }
