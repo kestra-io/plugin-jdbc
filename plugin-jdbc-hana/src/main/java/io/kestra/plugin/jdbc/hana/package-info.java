24393c16dbc4567981eee11c5bd2414093aa1b36
suraj a <96696731+suraj-0628@users.noreply.github.com>
2026-01-16 15:41:51 +0530

    feat(hana) : add SAP HANA support (#753)
    * chore(version): update to version 'v1.3.1'.
    
    * chore(version): update snapshot version 'v1.3.2-SNAPSHOT'.
    
    * test(duckdb): add DuckDbTestUtils helper for uploading CSV and DB file
    
    * added icon and moved hana.yaml to metadata
    
    * Add SVG icon for JDBC HANA plugin
    
    * resolved all suggestions
    
    * Revert "chore(deps): bump com.vanniktech.maven.publish from 0.34.0 to 0.35.0 (#736)"
    
    This reverts commit 35d9802f9ac41b4dfc5ab5b8fac2c775d8d1350e.
    
    * added tests
    
    ---------
    
    Co-authored-by: François Delbrayelle <fdelbrayelle@gmail.com>

---
+++ plugin-jdbc-hana/src/main/java/io/kestra/plugin/jdbc/hana/package-info.java
@@ -0,0 +1,7 @@
+@PluginSubGroup(
+    description = "Tasks that query SAP HANA using the official SAP HANA JDBC driver (ngdbc.jar).",
+    categories = PluginSubGroup.PluginCategory.DATABASE
+)
+package io.kestra.plugin.jdbc.hana;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
