119c5f49bf0edc1c6edb5317118aa12b09d82476
Nancy <9d.24.nancy.sangani@gmail.com>
2026-09-24 11:45:33 +0530

    fix(dremio): set io.netty.tryReflectionSetAccessible to avoid SSL negotiation failure

--- plugin-jdbc-dremio/src/main/java/io/kestra/plugin/jdbc/dremio/Queries.java
+++ plugin-jdbc-dremio/src/main/java/io/kestra/plugin/jdbc/dremio/Queries.java
@@ -66,6 +66,9 @@ public class Queries extends AbstractJdbcQueries implements DremioConnectionInte
 
     @Override
     public void registerDriver() throws SQLException {
+        // Netty needs this to use direct buffers on JDK9+, else TLS handshake fails
+        System.setProperty("io.netty.tryReflectionSetAccessible", "true");
+
         // only register the driver if not already exist to avoid a memory leak
         if (DriverManager.drivers().noneMatch(Driver.class::isInstance)) {
             DriverManager.registerDriver(new Driver());
